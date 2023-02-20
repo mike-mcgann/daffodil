9f84db8731e7c6fa6ca38a64608c5b8f0ddf59a9
Mike McGann <mmcgann@owlcyberdefense.com>
2023-02-20 09:52:01 -0500

    test case from mil-std-2045

--- daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestChoice.scala
+++ daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestChoice.scala
@@ -160,9 +160,8 @@ class TestChoice {
   @Test def test_direct_dispatch_16(): Unit = { runnerCH.runOneTest("direct_dispatch_16") }
   @Test def test_direct_dispatch_17(): Unit = { runnerCH.runOneTest("direct_dispatch_17") }
 
-  @Test def test_choiceFlagEven(): Unit = { runnerCH.runOneTest("choiceFlagEven") }
-  @Test def test_choiceFlagOdd(): Unit = { runnerCH.runOneTest("choiceFlagOdd") }
-  @Test def test_choiceFlagChar(): Unit = { runnerCH.runOneTest("choiceFlagChar") }
+  @Test def test_presenceByChoice1(): Unit = { runnerCH.runOneTest("presenceByChoice1") }
+  @Test def test_presenseByChoice2(): Unit = { runnerCH.runOneTest("presenceByChoice2") }
 
   // DAFFODIL-2562
   // @Test def test_dispatch_group_choice(): Unit = { runnerCH.runOneTest("dispatch_group_choice") }
---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestPresenceByChoice.scala
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.section15.choice_groups
+
+import org.apache.daffodil.tdml.Runner
+
+import org.junit.AfterClass
+import org.junit.Test
+
+object TestPresenceByChoice {
+  val testDir = "/org/apache/daffodil/section15/choice_groups/"
+
+  val runner = Runner(testDir, "PresenceByChoice.tdml")
+
+  @AfterClass def shutDown(): Unit = {
+    runner.reset
+  }
+
+}
+
+class TestPresenceByChoice {
+
+  import TestPresenceByChoice._
+
+  @Test def test_presenceByChoice1(): Unit = {
+    runner.runOneTest("presenceByChoice1")
+  }
+
+  @Test def test_presenceByChoice2(): Unit = {
+    runner.runOneTest("presenceByChoice2")
+  }
+}
+
