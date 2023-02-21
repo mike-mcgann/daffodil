ad5a378081208b294677a5536b91dc415eb8ef02
Mike McGann <mmcgann@owlcyberdefense.com>
2023-02-21 12:59:00 -0500

    choice branch tests

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestChoiceBranches.scala
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.section15.choice_groups
+
+import org.apache.daffodil.tdml.Runner
+
+import org.junit.AfterClass
+import org.junit.Test
+
+object TestChoiceBranches {
+  val testDir = "/org/apache/daffodil/section15/choice_groups/"
+
+  val runner = Runner(testDir, "ChoiceBranches.tdml")
+
+  @AfterClass def shutDown(): Unit = {
+    runner.reset
+  }
+
+}
+
+class TestChoiceBranches {
+
+  import TestChoiceBranches._
+
+  @Test def test_choiceBranch_e1_empty_selectFirst(): Unit = {
+    runner.runOneTest("choiceBranch_e1_empty_selectFirst")
+  }
+
+  @Test def test_choiceBranch_e1_notEmpty_selectSecond(): Unit = {
+    runner.runOneTest("choiceBranch_e1_notEmpty_selectSecond")
+  }
+
+  @Test def test_choiceBranch_e2_notEmpty_selectFirst(): Unit = {
+    runner.runOneTest("choiceBranch_e2_notEmpty_selectFirst")
+  }
+
+  @Test def test_choiceBranch_e2_empty_selectFirst(): Unit = {
+    runner.runOneTest("choiceBranch_e2_notEmpty_selectFirst")
+  }
+}
